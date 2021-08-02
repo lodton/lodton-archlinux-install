08b38ce924d3e119042304cdf7256cc40568b95f
lodton <954796494@qq.com>
2021-08-03 05:56:03 +0800

    Update b.sh

--- b.sh
+++ b.sh
@@ -1,6 +1,6 @@
 pacman -S su sudo amd-ucode dosfstools man  
 pacman -S ttf-dejavu wqy-microhei
 pacman -S fcitx-im fcitx-configtool fcitx-sunpinyin
-pacman -S zsh chromium vlc 
+pacman -S zsh chromium vlc uget
 #audio
 #pacman -S alsa-utils pulseaudio pulseaudio-alsa 
