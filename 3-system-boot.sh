7de418c14a66fc471c28758525567dfc6b00e69a
lodton <954796494@qq.com>
2021-08-03 06:00:23 +0800

    Update and rename system-boot.sh to 3-system-boot.sh

--- 3-system-boot.sh
+++ 3-system-boot.sh
@@ -1,3 +1,7 @@
 pacman -S grub efibootmgr
 grub-install --target=x86_64-efi --efi-directory=/boot/EFI  --recheck --bootloader-id=ArchLinux
 grub-mkconfig -o /boot/grub/grub.cfg
+
+
+systemctl enable dhcpcd 
+systemctl enable networkmanager
