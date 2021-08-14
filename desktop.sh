28d03209c0303fc0db16f95ed2729711fd88b603
lodton <lodton@outlook.com>
2021-08-14 09:52:58 +0800

    Update desktop.sh

--- desktop.sh
+++ desktop.sh
@@ -1,1 +1,18 @@
+#xorg
+pacman -S xorg
 
+#video
+#pacman -S xf86-video-amdgpu
+#pacman -S xf86-video-intel
+pacman -S nvidia
+
+#i3
+#pacman -S gdm i3
+
+pacman -S sddm i3
+
+#gnome
+#pacman -S gnome
+
+#kde
+#pacman -S plasma kde-applications
