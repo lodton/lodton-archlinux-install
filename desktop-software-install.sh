da7c6772e1b5f47221b364075efb7e27b3bb51ff
lodton <lodton@outlook.com>
2021-08-14 09:52:21 +0800

    Update desktop-software-install.sh

--- desktop-software-install.sh
+++ desktop-software-install.sh
@@ -1,22 +1,3 @@
-#xorg
-pacman -S xorg
-
-#video
-#pacman -S xf86-video-amdgpu
-#pacman -S xf86-video-intel
-pacman -S nvidia
-
-#i3
-#pacman -S gdm i3
-
-pacman -S sddm i3
-
-#gnome
-#pacman -S gnome
-
-#kde
-#pacman -S plasma kde-applications
-
 pacman -S su sudo amd-ucode dosfstools man
 
 pacman -S ttf-dejavu wqy-microhei
