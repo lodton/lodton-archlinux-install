3df03b5c53258c04c997f8f13b4362b61024f47c
lodton <lodton@outlook.com>
2021-08-14 09:42:56 +0800

    Update and rename software-installation.sh to desktop-software-installation.sh

---
+++ desktop-software-installation.sh
@@ -0,0 +1,23 @@
+#xorg
+pacman -S xorg
+
+#i3
+#pacman -S gdm i3
+
+#pacman -S sddm i3
+
+#gnome
+#pacman -S gnome
+
+#kde
+#pacman -S plasma kde-applications
+
+pacman -S su sudo amd-ucode dosfstools man
+
+pacman -S ttf-dejavu wqy-microhei
+
+pacman -S fcitx-im fcitx-configtool fcitx-sunpinyin
+pacman -S zsh chromium vlc uget
+
+#audio
+#pacman -S alsa-utils pulseaudio pulseaudio-alsa 
