365483419d4e0d593588b328432c3da412459801
lodton <954796494@qq.com>
2021-08-28 07:49:44 +0800

    更新
    更新

--- software.sh
+++ software.sh
@@ -4,7 +4,7 @@ pacman -S ttf-dejavu wqy-microhei
 
 pacman -S fcitx-im fcitx-configtool fcitx-sunpinyin
 
-pacman -S zsh chromium vlc uget
+pacman -S chromium vlc uget
 
 #audio
 #pacman -S alsa-utils pulseaudio pulseaudio-alsa 
--- zsh/zsh-config-install.sh
+++ zsh/zsh-config-install.sh
@@ -1,3 +1,4 @@
+pacman -S zsh
 pacman -S zsh-completions  #命令补全插件
 pacman -S zsh-autosuggestions  #这个是自动建议插件，能够自动提示你需要的命令。
 pacman -S zsh-syntax-highlighting  #这个是代码高亮插件，能够使你的命令行各个命令清晰明了。
