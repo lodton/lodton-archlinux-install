0262690a7fad9906503c33d001c2e37184224808
lodton <954796494@qq.com>
2021-09-10 09:43:23 +0800

    更新

--- pacstrap.sh
+++ pacstrap.sh
@@ -1,4 +1,4 @@
-pacman -Syyu
+pacman -Syy
 pacstrap -i /mnt base base-devel linux linux-firmware vim nano dhcpcd networkmanager iwd 
 genfstab -U /mnt >> /mnt/etc/fstab
 cat /mnt/etc/fstab
--- system-boot.sh
+++ system-boot.sh
@@ -1,7 +1,10 @@
+pacman -Syy
 pacman -S grub efibootmgr
 grub-install --target=x86_64-efi --efi-directory=/boot/EFI  --recheck --bootloader-id=ArchLinux
 grub-mkconfig -o /boot/grub/grub.cfg
 
 
+systemctl start dhcpcd 
 systemctl enable dhcpcd 
+systemctl start networkmanager
 systemctl enable networkmanager
